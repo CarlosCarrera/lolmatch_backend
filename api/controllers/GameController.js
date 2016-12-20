4a767ee28006345e85b48bc309e12100de032b55
Carlos Carrera <carlos.carrera.altran@axa-groupsolutions.com>
2016-12-20 10:12:25 +0100

    Fixed GameController

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -9,19 +9,16 @@ var request = require('request');
 var querystring = require('querystring');
 var baseUrl = 'https://euw.api.pvp.net/';
 
-
 var gameServices = {
 	featuredGame: function(req,res) {
 		var options = {
-			url: '/observer-mode/rest/featured',
-			baseUrl: baseUrl,
-			qs: querystring.stringify({api_key: 'ee91b462-2a25-4dd6-a14f-9ff4adadf092'}),
+			url: baseUrl + '/observer-mode/rest/featured?' +
+			querystring.stringify({api_key: 'ee91b462-2a25-4dd6-a14f-9ff4adadf092'}),
 			method: 'GET'
 		};
 		request(options,function(error,response,body){
-			console.log(response.statusCode);
 			if (!error && response.statusCode == 200) {
-				console.log(body);
+				res.send(body);
 			}
 		});
 	},
